e7068b3a95a7e1f7cecfce59d2df3410c9c2e8ee
rdebruyker <rdebruyker@cbtw.tech>
2024-07-17 17:00:41 +0200

    Read an existing csv file and work from it. Read all emails given in input/talkwalker and update df accordingly. Works without bug but duplicates to be handled and have to check for news.

--- src/read_email.py
+++ src/read_email.py
@@ -1,5 +1,6 @@
 import re
 import os
+import glob
 from datetime import datetime
 from email import policy
 from email.parser import BytesParser
@@ -27,37 +28,39 @@ def extract_links_from_talkwalker(eml_file_path):
             twitter_index = i
 
     # Extract the content between NEWS and TWITTER
-    news_content = comments[news_index].find_next_sibling()
-    elements = []
-    while news_content and news_content != comments[twitter_index]:
-        elements.append(news_content)
-        news_content = news_content.find_next_sibling()
+    if news_index and twitter_index:
+        news_content = comments[news_index].find_next_sibling()
+        elements = []
+        while news_content and news_content != comments[twitter_index]:
+            elements.append(news_content)
+            news_content = news_content.find_next_sibling()
 
-    # Prepare data for DataFrame
-    data = []
-    for element in elements:
-        if element.name == 'tr':
-            title = element.find('a').text if element.find('a') else None
-            source = element.find('a')['href'] if element.find('a') else None
-            summary = None
-            date = None
-            state = None
-            website = None
-            for td in element.find_all('td'):
-                date_state_website = td.text.strip().split(' | ')
-                if td.text.strip()[:3]=="..."  and  td.text.strip()[-3:]=="..." :
-                    summary = td.text.strip()
-                elif len(date_state_website) == 3:
-                    if len(date_state_website[0]) < 15:
-                        date = datetime.strptime(date_state_website[0], '%d.%m.%y %H:%M')
-                        state = date_state_website[1]
-                        website = date_state_website[2]
-            if title and "alerts.talkwalker.com" not in source and date:
-                data.append([title, source, summary, date, state, website])
+        # Prepare data for DataFrame
+        data = []
+        for element in elements:
+            if element.name == 'tr':
+                title = element.find('a').text if element.find('a') else None
+                source = element.find('a')['href'] if element.find('a') else None
+                summary = None
+                date = None
+                state = None
+                website = None
+                for td in element.find_all('td'):
+                    date_state_website = td.text.strip().split(' | ')
+                    if td.text.strip()[:3]=="..."  and  td.text.strip()[-3:]=="..." :
+                        summary = td.text.strip()
+                    elif len(date_state_website) == 3:
+                        if len(date_state_website[0]) < 15:
+                            date = datetime.strptime(date_state_website[0], '%d.%m.%y %H:%M')
+                            state = date_state_website[1]
+                            website = date_state_website[2]
+                if title and "alerts.talkwalker.com" not in source and date:
+                    data.append([title, source, summary, date, state, website])
 
-    # Create DataFrame
-    df = pd.DataFrame(data, columns=COLUMNS)
-    return df
+        # Create DataFrame
+        df = pd.DataFrame(data, columns=COLUMNS)
+        return df
+    return None
 
 def remove_non_articles(df):
     #df = df.dropna(subset=['Title', 'Source', 'Summary'], how="all")
@@ -88,16 +91,30 @@ def extract_links_from_eml(eml_file_path):
     
     return links
 
-# Call the function and print the extracted links
-eml_file_path = './input/talkwalker/talkwalker.eml'  # Replace with the actual eml file path
-#extracted_links = extract_links_from_eml(eml_file_path)
-df = extract_links_from_talkwalker(eml_file_path)
-#df = remove_non_articles(df)
-save_csv(df, "./output/alerts.csv")
+def main():# Call the function and print the extracted links
+    output_file = "./output/alerts.csv"
+    if os.path.exists(output_file):
+        df = pd.read_csv(output_file)
+    else:
+        df = pd.DataFrame(columns=COLUMNS)
+    input_folder = "./input"
+    talkwalker_folder = os.path.join(input_folder, "talkwalker")
+    print(talkwalker_folder)
+    for eml_file_path in glob.glob(os.path.join(talkwalker_folder, "*")):
+        print(eml_file_path)
+        df_new = extract_links_from_talkwalker(eml_file_path)
+        df_new = remove_non_articles(df_new)
+        df = pd.concat([df, df_new], ignore_index=True)
+        df = df.drop_duplicates()
+    save_csv(df, "./output/alerts.csv")
 
-# TODO remove useless
-# TODO fix summary
-# TODO go into the website and check for the mention?? 
+    # TODO remove useless
+    # TODO fix summary
+    # TODO go into the website and check for the mention?? 
+
+main()
+
+
 
 
 
